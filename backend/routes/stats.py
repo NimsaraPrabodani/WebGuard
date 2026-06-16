7a18524978bd8e396da3a192f2ad123b2be176f0
shabodathebuwana <shabodathebuwana@gmail.com>
2026-06-16 12:34:44 +0530

    Secure backend with .env for MongoDB

--- backend/routes/stats.py
+++ backend/routes/stats.py
@@ -5,13 +5,36 @@ stats_bp = Blueprint("stats_bp", __name__)
 
 @stats_bp.route("/stats", methods=["GET"])
 def stats():
-    total = collection.count_documents({})
-    safe = collection.count_documents({"result.status": "Safe Website"})
-    phishing = collection.count_documents({"result.status": "Phishing Website"})
+    data =  collection.find()
+
+    safe = 0
+    suspicious = 0
+    dangerous = 0
+    total = 0
+
+    for i in data:
+        total += 1
+
+        status = i.get("status", "")
+
+        if status == "Safe":
+            safe += 1
+        elif status == "Suspicious":
+            suspicious += 1
+        elif status == "Dangerous":
+            dangerous+= 1
+
+    safe_pct = (safe / total) * 100 if total > 0 else 0
+    suspicious_pct = (suspicious / total) * 100 if total > 0 else 0
+    dangerous_pct = (dangerous/ total) * 100 if total > 0 else 0
 
 
     return jsonify({
         "total": total,
         "safe": safe,
-        "phishing": phishing
+        "suspicious": suspicious,
+        "dangerous": dangerous,
+        "safe_percentage" : round(safe_pct,2),
+        "suspicious_percentage" : round(suspicious_pct, 2),
+        "dangerous_percentage" : round(dangerous_pct, 2)
     })
